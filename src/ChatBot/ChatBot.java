e6c3ee567be7a6dc93af15fc68994aa013656dc5
maksim <filmaksim02@gmail.com>
2021-09-30 15:37:17 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Amogus.");
         System.out.println("I was created in 2021.");
+        System.out.println("Please, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String name_user = scanner.nextLine();
+        System.out.format("What a great name you have, %s!",name_user);
     }
 }
